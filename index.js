e4faa728eeb9ddb0b52ddd5759d6e51768f16b27
jnErfan <j.n.erfan420@gmail.com>
2021-10-31 02:04:08 +0600

    Pagenation

--- index.js
+++ index.js
@@ -1,84 +1,113 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send(`<h1  style="text-align: center; margin-top:100px;  font-weight: 900; color: blue">Welcome To Fantasy Kingdom Backend Server</h1>`)
-})
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyw7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const database = client.db('fantasyData');
-    const ridesCollection = database.collection('ridesPackage');
-    const customerCollection = database.collection('customerInfo');
+app.get("/", (req, res) => {
+  res.send(
+    `<h1  style="text-align: center; margin-top:100px;  font-weight: 900; color: blue">Welcome To Fantasy Kingdom Backend Server</h1>`
+  );
+});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyw7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const database = client.db("fantasyData");
+  const ridesCollection = database.collection("ridesPackage");
+  const customerCollection = database.collection("customerInfo");
 
-    app.get('/ridesPackageHome' , async (req,res)=>{
-            const ridePackagesHome = await ridesCollection.find({}).limit(9).toArray();
-            res.send(ridePackagesHome);
-    })
+  app.get("/ridesPackageHome", async (req, res) => {
+    const ridePackagesHome = await ridesCollection.find({}).limit(9).toArray();
+    res.send(ridePackagesHome);
+  });
 
+  // Get Single Id Package
+  app.get("/rides/:id", async (req, res) => {
+    const params = req.params.id;
+    const query = { _id: ObjectId(params) };
+    const result = await ridesCollection.find(query).toArray();
+    res.send(result);
+  });
 
-    // Get Single Id Package 
-    app.get("/rides/:id", async (req, res) => {
-        const params = req.params.id;
-        const query = {_id:ObjectId(params)};
-        const result = await ridesCollection.find(query).toArray();
-        res.send(result);
-    }) 
+  app.get("/rides", async (req, res) => {
+    const ridePackages = await ridesCollection.find({}).toArray();
+    res.send(ridePackages);
+  });
+  app.post("/rides", async (req, res) => {
+    const package = req.body;
+    const result = await ridesCollection.insertOne(package);
+    res.send(result);
+    console.log(result);
+  });
 
-    app.get('/rides' , async (req,res)=>{
-        const ridePackages = await ridesCollection.find({}).toArray();
-        res.send(ridePackages)
-    })
-    app.post("/rides", async (req,res)=>{
-        const package = req.body;
-        const result = await ridesCollection.insertOne(package);
-        res.send(result);
-        console.log(result);
-    })
+  // Pagination
+  app.get("/pagination", async (req, res) => {
+    const ridesUi = ridesCollection.find({});
+    const page = req.query.page;
+    const size = parseInt(req.query.size);
+    let ridesPackage;
+    const count = await ridesUi.count();
+    if (page) {
+      ridesPackage = await ridesUi
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+    } else {
+      ridesPackage = await ridesUi.toArray();
+    }
+    res.send({ count, ridesPackage });
+  });
 
+  // Buyer Information Post
+  app.post("/orderInfo", async (req, res) => {
+    const info = req.body;
+    const result = await customerCollection.insertOne(info);
+    res.send(result);
+  });
+  app.delete("/deleteOrder/:id", async (req, res) => {
+    const params = req.params.id;
+    const query = { _id: ObjectId(params) };
+    const result = await customerCollection.deleteOne(query);
+    res.send(result);
+    console.log(result);
+  });
+  app.get("/allOrders", async (req, res) => {
+    const ridePackages = await customerCollection.find({}).toArray();
+    res.send(ridePackages);
+  });
 
-    // Buyer Information Post 
-    app.post("/orderInfo", async (req,res)=>{
-        const info = req.body;
-        const result = await customerCollection.insertOne(info);
-        res.send(result);
-    })
-    app.get('/allOrders' , async (req,res)=>{
-        const ridePackages = await customerCollection.find({}).toArray();
-        res.send(ridePackages)
-    })
+  app.put("/orderStatus/:id", async (req, res) => {
+    const params = req.params.id;
+    const updateId = { _id: ObjectId(params) };
+    const query = req.body;
+    const updateDoc = {
+      $set: {
+        status: query.status,
+        rejectReason: query.rejectReason,
+      },
+    };
+    const result = await customerCollection.updateOne(updateId, updateDoc);
+    res.json(result);
+  });
 
-    app.put("/orderStatus/:id", async (req,res)=> {
-        const params = req.params.id;
-        const updateId = {_id:ObjectId(params)}
-        const query = req.body;
-        const updateDoc = {
-            $set: {
-                status : query.status,
-                rejectReason : query.rejectReason
-            },
-          };
-          const result = await customerCollection.updateOne(updateId, updateDoc);
-          res.json(result)
-    })
-
-    app.get('/matchPackage/:email', async (req, res) => {
-        const emailMatchPackage =  req.params.email;
-      const result = await customerCollection.find({email: emailMatchPackage}).toArray();
-      res.send(result)
-      });
-//   client.close();
+  app.get("/matchPackage/:email", async (req, res) => {
+    const emailMatchPackage = req.params.email;
+    const result = await customerCollection
+      .find({ email: emailMatchPackage })
+      .toArray();
+    res.send(result);
+  });
+  //   client.close();
 });
 
-  
-
-app.listen(port, ()=>{console.log('Server Is Running With Port', port)})
+app.listen(port, () => {
+  console.log("Server Is Running With Port", port);
+});
